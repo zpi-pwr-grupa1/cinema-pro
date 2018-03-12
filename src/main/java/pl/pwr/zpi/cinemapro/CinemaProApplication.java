f103d515a346d3ac32fe5fe7fdadcea7649831d0
hfudali <hubert.fudali@o2.pl>
2018-03-12 01:34:44 +0100

    initial project setup

---
+++ src/main/java/pl/pwr/zpi/cinemapro/CinemaProApplication.java
@@ -0,0 +1,12 @@
+package pl.pwr.zpi.cinemapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaProApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/pwr/zpi/cinemapro/CinemaProApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.pwr.zpi.cinemapro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinemaProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
