52b046f4cb91b836ceb8b63a0edc09512188df97
hfudali <hubert.fudali@o2.pl>
2018-03-21 03:01:37 +0100

    CORS enabled

--- src/main/java/pl/pwr/zpi/cinemapro/common/config/WebMvcConfig.java
+++ src/main/java/pl/pwr/zpi/cinemapro/common/config/WebMvcConfig.java
@@ -3,14 +3,20 @@ package pl.pwr.zpi.cinemapro.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pl.pwr.zpi.cinemapro.common.util.DTOModelMapper;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+
+// FIXME find alternatives for deprecated classes
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -28,5 +34,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         super.addArgumentResolvers(argumentResolvers);
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().applicationContext(this.applicationContext).build();
         argumentResolvers.add(new DTOModelMapper(objectMapper, entityManager));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000"); //FIXME hardcoded value
+            }
+        };
     }
 }
