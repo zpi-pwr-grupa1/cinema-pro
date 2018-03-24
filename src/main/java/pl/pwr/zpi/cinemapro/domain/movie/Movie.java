06e7279ae2b4a0c3a4ef91aa3658e5206b22d1d7
Damian <damian.miara93@gmail.com>
2018-03-24 10:01:50 +0100

    Deleted comments
    Usunąłem komentarze z klasy Movie

--- src/main/java/pl/pwr/zpi/cinemapro/domain/movie/Movie.java
+++ src/main/java/pl/pwr/zpi/cinemapro/domain/movie/Movie.java
@@ -15,49 +15,38 @@ public class Movie {
     @GeneratedValue
     private UUID id;
 
-    //tytuł
     @Column(unique = true, nullable = false)
     private String title;
 
-    //dopuszczalna grupa wiekowa
     @Column(nullable = true)
     private String age;
 
-    //miejsce powstania
     @Column(nullable = true)
     private String country;
     
-    //czas trwania w minutach (wygodniej wpisać i przedstawić 90min, niż 01:30:00)
     @Column(nullable = true)
     private int runTime;
 
-    //premiera polska
     @Column(nullable = true)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date polishReleaseDate;
     
-    //premiera światowa
     @Column(nullable = true)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date worldReleaseDate;
     
-    //krótki opis fabuły
     @Column(nullable = true)
     private String storyline;
     
-    //url do zdjęcia
     @Column(nullable = true)
     private String imgURL;
     
-    //reżyser
     @Column(nullable = true)
     private String director;
     
-    //obsada
     @Column(nullable = true)
     private String movieCast;
     
-    //widoczność
     @Column(nullable = false)
     @Value("true")
     private boolean visible;
