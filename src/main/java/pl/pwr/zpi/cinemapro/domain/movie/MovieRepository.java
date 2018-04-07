047048a290c08034eb9f8cd16ddd29fe19a10375
Pawel Trukawka <trukawp@gmail.com>
2018-04-07 23:48:09 +0200

    Dodano getID do Movie

--- src/main/java/pl/pwr/zpi/cinemapro/domain/movie/MovieRepository.java
+++ src/main/java/pl/pwr/zpi/cinemapro/domain/movie/MovieRepository.java
@@ -3,5 +3,12 @@ package pl.pwr.zpi.cinemapro.domain.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {}
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+  Movie findById(UUID uuid);
+
+}
--- src/main/java/pl/pwr/zpi/cinemapro/domain/movie/MovieService.java
+++ src/main/java/pl/pwr/zpi/cinemapro/domain/movie/MovieService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MovieService {
@@ -24,4 +25,7 @@ public class MovieService {
         movieRepository.save(movie);
     }
 
+    public Movie findById(UUID id){
+        return movieRepository.findById(id);
+    }
 }
