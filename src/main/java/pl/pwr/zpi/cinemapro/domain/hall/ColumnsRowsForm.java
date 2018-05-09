bf4bb35ec2ced2ca5e07a478ba1d580ba1d8f3c4
Macioosio <strusiek148@gmail.com>
2018-05-09 18:55:21 +0200

    Added form for accessing rows and columns.

---
+++ src/main/java/pl/pwr/zpi/cinemapro/domain/hall/ColumnsRowsForm.java
@@ -0,0 +1,13 @@
+package pl.pwr.zpi.cinemapro.domain.hall;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ColumnsRowsForm {
+
+    private int rows;
+
+    private int columns;
+}
