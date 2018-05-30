c7d7c21a750c8b4d53027d20a9af9175b92d4306
Macioosio <strusiek148@gmail.com>
2018-05-30 21:22:10 +0200

    Added Movies and Showings testdata.

--- src/main/java/pl/pwr/zpi/cinemapro/common/initialization/DataInitializer.java
+++ src/main/java/pl/pwr/zpi/cinemapro/common/initialization/DataInitializer.java
@@ -88,8 +88,7 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         
         MovieGroup g2 = new MovieGroup();
         g2.setLabel("Action");
-        
-        
+
         TicketType tt1 = new TicketType();
         tt1.setName("Normalny");
         tt1.setPrice(20.0);
@@ -147,6 +146,62 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         m3.setVisible(true);
         m3.setGroups(Sets.newHashSet(g2));
 
+        Movie m4 = new Movie();
+        m4.setAge("PG-18");
+        m4.setCountry("USA");
+        m4.setDirector("Anthony Russo, Joe Russo");
+        m4.setMovieCast("Tom Holland, Chris Hemsworth, Robert Downey Jr., Mark Ruffalo, Chris Evans");
+        m4.setPolishReleaseDate(new Date());
+        m4.setWorldReleaseDate(new Date());
+        m4.setRunTime(133);
+        m4.setStoryline("Having acquired the Power Stone from the planet Xandar, Thanos and his lieutenants—Ebony Maw, Cull Obsidian, Proxima Midnight, and Corvus Glaive—intercept the spaceship carrying the survivors of Asgard's destruction. ");
+        m4.setTitle("Avengers: Infinity War");
+        m4.setImgURL("/assets/images/movies/avengers.jpg");
+        m4.setVisible(true);
+        m4.setGroups(Sets.newHashSet(g2, g1));
+
+        Movie m5 = new Movie();
+        m5.setAge("PG-12");
+        m5.setCountry("USA");
+        m5.setDirector("David Leitch");
+        m5.setMovieCast("Ryan Reynolds, Josh Brolin, Morena Baccarin");
+        m5.setPolishReleaseDate(new Date());
+        m5.setWorldReleaseDate(new Date());
+        m5.setRunTime(124);
+        m5.setStoryline("After successfully working as the mercenary Deadpool for two years, Wade Wilson fails to kill one of his targets on his anniversary with his girlfriend Vanessa. ");
+        m5.setTitle("Deadpool 2");
+        m5.setImgURL("/assets/images/movies/deadpool.jpg");
+        m5.setVisible(true);
+        m5.setGroups(Sets.newHashSet(g1, g2));
+
+        Movie m6 = new Movie();
+        m6.setAge("PG-12");
+        m6.setCountry("USA");
+        m6.setDirector("Francis Ford Coppola");
+        m6.setMovieCast("Marlon Brando, Al Pacino");
+        m6.setPolishReleaseDate(new Date());
+        m6.setWorldReleaseDate(new Date());
+        m6.setRunTime(177);
+        m6.setStoryline("In 1945, at his daughter Connie's wedding, Vito Corleone hears requests in his role as the Godfather, the Don of a New York crime family. Vito's youngest son ...");
+        m6.setTitle("The Godfather");
+        m6.setImgURL("/assets/images/movies/godfather.jpg");
+        m6.setVisible(true);
+        m6.setGroups(Sets.newHashSet(g2));
+
+        Movie m7 = new Movie();
+        m7.setAge("PG-12");
+        m7.setCountry("USA");
+        m7.setDirector("Steven Spielberg");
+        m7.setMovieCast("Sam Neill, Laura Dern, Jeff Goldblum, Richard Attenborough");
+        m7.setPolishReleaseDate(new Date());
+        m7.setWorldReleaseDate(new Date());
+        m7.setRunTime(127);
+        m7.setStoryline("Industrialist John Hammond and his bioengineering company, InGen, have created a theme park called Jurassic Park on Isla Nublar, a Costa Rican island, populated with cloned dinosaurs.");
+        m7.setTitle("Jurassic Park");
+        m7.setImgURL("/assets/images/movies/godfather.jpg");
+        m7.setVisible(true);
+        m7.setGroups(Sets.newHashSet(g2));
+
         Cinema c1 = new Cinema();
         c1.setName("CinemaPro Grabiszynska");
         c1.setStreet("Grabiszynska");
@@ -201,14 +256,14 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         h3.setHallNumber(1);
         HashSet<Seat> seats3 = new HashSet<>(seatService.createSeats(15, 20));
         h3.setSeats(seats3);
-        h3.setCinema(c2);
+        h3.setCinema(c1);
         h3.setVisible(true);
 
         Hall h4 = new Hall();
         h4.setHallNumber(2);
         HashSet<Seat> seats4 = new HashSet<>(seatService.createSeats(14, 13));
         h4.setSeats(seats4);
-        h4.setCinema(c2);
+        h4.setCinema(c1);
         h4.setVisible(true);
 
         Hall h5 = new Hall();
@@ -217,6 +272,13 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         h5.setSeats(seats5);
         h5.setCinema(c3);
         h5.setVisible(true);
+
+        Hall h6 = new Hall();
+        h6.setHallNumber(12);
+        HashSet<Seat> seats6 = new HashSet<>(seatService.createSeats(14, 13));
+        h6.setSeats(seats5);
+        h6.setCinema(c2);
+        h6.setVisible(true);
 
         Ticket t1 = new Ticket();
         Seat seat = seats.iterator().next();
@@ -233,66 +295,207 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         t3.setSeat(seat);
         t3.setTicketType(tt3);
 
-        Showing s1 = new Showing();
-        s1.setScreeningStart(new Date());
-        s1.setHall(h1);
-        s1.setMovie(m1);
+        Date todayDate = new Date();
+        Calendar todayCalendar = Calendar.getInstance();
+        todayCalendar.setTime(todayDate);
+        int currentYear = todayCalendar.get(Calendar.YEAR);
+        int currentMonth = todayCalendar.get(Calendar.MONTH);
+        int currentDay = todayCalendar.get(Calendar.DAY_OF_MONTH);
 
         Calendar cal = Calendar.getInstance();
 
-        Showing s2 = new Showing();
-        cal.set(2053, 1, 1, 15, 15, 00);
-        s2.setScreeningStart(cal.getTime());
-        s2.setHall(h2);
-        s2.setMovie(m2);
+        Showing s1 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 9, 00, 00);
+        s1.setScreeningStart(cal.getTime());
+        s1.setHall(h1);
+        s1.setMovie(m1);
 
-        Showing s5 = new Showing();
-        cal.set(2053, 1, 1, 17, 15, 00);
-        s5.setScreeningStart(cal.getTime());
-        s5.setHall(h2);
-        s5.setMovie(m2);
+        Showing s2 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 12, 00, 00);
+        s2.setScreeningStart(cal.getTime());
+        s2.setHall(h1);
+        s2.setMovie(m1);
 
         Showing s3 = new Showing();
-        cal.set(2053, 1, 1, 15, 30, 00);
+        cal.set(currentYear, currentMonth, currentDay, 15, 00, 00);
         s3.setScreeningStart(cal.getTime());
         s3.setHall(h1);
-        s3.setMovie(m3);
+        s3.setMovie(m1);
 
         Showing s4 = new Showing();
-        cal.set(2053, 1, 1, 10, 15, 00);
+        cal.set(currentYear, currentMonth, currentDay, 18, 15, 00);
         s4.setScreeningStart(cal.getTime());
-        s4.setHall(h2);
-        s4.setMovie(m3);
+        s4.setHall(h1);
+        s4.setMovie(m1);
+
+        Showing s5 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 9, 00, 00);
+        s5.setScreeningStart(cal.getTime());
+        s5.setHall(h1);
+        s5.setMovie(m1);
 
         Showing s6 = new Showing();
-        cal.set(2053, 1, 1, 15, 15, 00);
+        cal.set(currentYear, currentMonth, currentDay + 1, 12, 00, 00);
         s6.setScreeningStart(cal.getTime());
-        s6.setHall(h3);
+        s6.setHall(h1);
         s6.setMovie(m1);
 
         Showing s7 = new Showing();
-        cal.set(2053, 1, 1, 17, 15, 00);
+        cal.set(currentYear, currentMonth, currentDay + 1, 15, 00, 00);
         s7.setScreeningStart(cal.getTime());
-        s7.setHall(h3);
-        s7.setMovie(m2);
+        s7.setHall(h1);
+        s7.setMovie(m1);
 
         Showing s8 = new Showing();
-        cal.set(2053, 1, 1, 15, 30, 00);
+        cal.set(currentYear, currentMonth, currentDay + 1, 18, 15, 00);
         s8.setScreeningStart(cal.getTime());
-        s8.setHall(h4);
-        s8.setMovie(m3);
+        s8.setHall(h1);
+        s8.setMovie(m1);
 
         Showing s9 = new Showing();
-        cal.set(2053, 1, 1, 13, 15, 00);
+        cal.set(currentYear, currentMonth, currentDay, 9, 00, 00);
         s9.setScreeningStart(cal.getTime());
-        s9.setHall(h4);
-        s9.setMovie(m3);
+        s9.setHall(h2);
+        s9.setMovie(m2);
 
         Showing s10 = new Showing();
-        cal.set(2053, 1, 1, 13, 15, 00);
+        cal.set(currentYear, currentMonth, currentDay, 12, 00, 00);
         s10.setScreeningStart(cal.getTime());
-        s10.setHall(h5);
-        s10.setMovie(m1);
+        s10.setHall(h2);
+        s10.setMovie(m2);
+
+        Showing s11 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 15, 00, 00);
+        s11.setScreeningStart(cal.getTime());
+        s11.setHall(h2);
+        s11.setMovie(m2);
+
+        Showing s12 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 18, 15, 00);
+        s12.setScreeningStart(cal.getTime());
+        s12.setHall(h2);
+        s12.setMovie(m2);
+
+        Showing s13 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 9, 00, 00);
+        s13.setScreeningStart(cal.getTime());
+        s13.setHall(h2);
+        s13.setMovie(m2);
+
+        Showing s14 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 12, 00, 00);
+        s14.setScreeningStart(cal.getTime());
+        s14.setHall(h2);
+        s14.setMovie(m2);
+
+        Showing s15 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 15, 00, 00);
+        s15.setScreeningStart(cal.getTime());
+        s15.setHall(h2);
+        s15.setMovie(m2);
+
+        Showing s16 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 18, 15, 00);
+        s16.setScreeningStart(cal.getTime());
+        s16.setHall(h2);
+        s16.setMovie(m2);
+
+        Showing s17 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 9, 00, 00);
+        s17.setScreeningStart(cal.getTime());
+        s17.setHall(h3);
+        s17.setMovie(m3);
+
+        Showing s18 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 12, 00, 00);
+        s18.setScreeningStart(cal.getTime());
+        s18.setHall(h3);
+        s18.setMovie(m3);
+
+        Showing s19 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 15, 00, 00);
+        s19.setScreeningStart(cal.getTime());
+        s19.setHall(h3);
+        s19.setMovie(m3);
+
+        Showing s20 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 18, 15, 00);
+        s20.setScreeningStart(cal.getTime());
+        s20.setHall(h3);
+        s20.setMovie(m3);
+
+        Showing s21 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 9, 00, 00);
+        s21.setScreeningStart(cal.getTime());
+        s21.setHall(h3);
+        s21.setMovie(m3);
+
+        Showing s22 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 12, 00, 00);
+        s22.setScreeningStart(cal.getTime());
+        s22.setHall(h3);
+        s22.setMovie(m3);
+
+        Showing s23 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 15, 00, 00);
+        s23.setScreeningStart(cal.getTime());
+        s23.setHall(h3);
+        s23.setMovie(m3);
+
+        Showing s24 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 18, 15, 00);
+        s24.setScreeningStart(cal.getTime());
+        s24.setHall(h3);
+        s24.setMovie(m3);
+
+        Showing s25 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 21, 00, 00);
+        s25.setScreeningStart(cal.getTime());
+        s25.setHall(h1);
+        s25.setMovie(m4);
+
+        Showing s26 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 22, 00, 00);
+        s26.setScreeningStart(cal.getTime());
+        s26.setHall(h2);
+        s26.setMovie(m4);
+
+        Showing s27 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 23, 15, 00);
+        s27.setScreeningStart(cal.getTime());
+        s27.setHall(h3);
+        s27.setMovie(m4);
+
+        Showing s28 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay, 9, 00, 00);
+        s28.setScreeningStart(cal.getTime());
+        s28.setHall(h4);
+        s28.setMovie(m5);
+
+        Showing s29 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 9, 00, 00);
+        s29.setScreeningStart(cal.getTime());
+        s29.setHall(h4);
+        s29.setMovie(m5);
+
+        Showing s30 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 12, 00, 00);
+        s30.setScreeningStart(cal.getTime());
+        s30.setHall(h4);
+        s30.setMovie(m5);
+
+        Showing s31 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 15, 00, 00);
+        s31.setScreeningStart(cal.getTime());
+        s31.setHall(h4);
+        s31.setMovie(m5);
+
+        Showing s32 = new Showing();
+        cal.set(currentYear, currentMonth, currentDay + 1, 18, 15, 00);
+        s32.setScreeningStart(cal.getTime());
+        s32.setHall(h4);
+        s32.setMovie(m5);
+
         
         Employee e1 = new Employee();
         cal.set(1999, 1, 1, 13, 15, 00);
@@ -351,10 +554,11 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
 
         groupRepository.saveAll(Lists.newArrayList(g1, g2));
         clientRepository.saveAll(Lists.newArrayList(cl1));
-        movieRepository.saveAll(Lists.newArrayList(m1, m2, m3));        
+        movieRepository.saveAll(Lists.newArrayList(m1, m2, m3, m4, m5, m6, m7));
         cinemaRepository.saveAll(Lists.newArrayList(c1, c2, c3));
         hallRepository.saveAll(Lists.newArrayList(h1, h2, h3, h4, h5));
-        showingRepository.saveAll(Lists.newArrayList(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10));
+        showingRepository.saveAll(Lists.newArrayList(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16,
+                s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29, s30, s31, s32));
         ticketTypeRepository.saveAll(Lists.newArrayList(tt1, tt2, tt3));
         ticketRepository.saveAll(Lists.newArrayList(t1, t2, t3));
         employeeRepository.saveAll(Lists.newArrayList(e1));
